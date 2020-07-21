3c4a576ea067ead277b98b1aa3630313635a8d45
Roch Goszczyński <roch.goszczynski@gmail.com>
2020-07-21 19:55:00 +0200

    add greedy pathfinding function

---
+++ src/util/algorithms/greedy.js
@@ -0,0 +1,6 @@
+import { basicAStar } from "./aStar";
+import { greedyHeuristic } from "./heuristics";
+
+export default function greedy(nodes, startNode, endNode) {
+    return basicAStar(nodes, startNode, endNode, greedyHeuristic);
+}
